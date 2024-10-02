a04dfd1b8a4dec62594e74d4871e2c7ad5dcb5a3
HOTHEAD01TH <hothead01th@gmail.com>
2024-10-02 22:28:19 +0530

    made some pages and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,9 @@
+import {  BrowserRouter,  Route,  Routes,} from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
+
 function App() {
   return (
     <>
@@ -12,3 +18,5 @@ function App() {
     </>
   )
 }
+
+export default App
